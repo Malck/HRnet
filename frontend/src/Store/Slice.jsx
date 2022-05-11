81b56620cb4fe39dbed5c7537e3884e2777609c9
MC <Malckira@gmail.com>
2022-05-11 15:09:54 +0200

    add mock

--- frontend/src/Store/Slice.jsx
+++ frontend/src/Store/Slice.jsx
@@ -1,16 +1,43 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+const isMock = true;
+const dataMock = [
+  {
+    firstName: "Joe",
+    lastName: "Loko",
+    birthDay: "1996-05-06",
+    startDate: "2004-12-25",
+    street: "mokez",
+    city: "fedfell",
+    zip: "23",
+    state: "FL",
+    department: "Legal",
+    id: "11154156",
+  },
+  {
+    firstName: "Adil",
+    lastName: "Bou",
+    birthDay: "1992-05-06",
+    startDate: "2004-08-10",
+    street: "zadar",
+    city: "oprie",
+    zip: "12",
+    state: "AS",
+    department: "Sales",
+    id: "11154256",
+  },
+];
 
 const userSlice = createSlice({
-    name: "userslice",
-    initialState: [],
-       
-    reducers:{
-        
-        addEmployee:(state,action) => {  //Ajoute un profil d'employee dans le store 
-            state.push(action.payload)     
-        }
-    }
-})
-export const { addEmployee } = userSlice.actions
+  name: "userslice",
+  initialState: isMock ? dataMock : [],
 
-export default userSlice.reducer
+  reducers: {
+    addEmployee: (state, action) => {
+      //Ajoute un profil d'employee dans le store
+      state.push(action.payload);
+    },
+  },
+});
+export const { addEmployee } = userSlice.actions;
+
+export default userSlice.reducer;
