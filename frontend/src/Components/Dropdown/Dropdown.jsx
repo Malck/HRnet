0a8d539a9df219d4bbe4ad97985cc34b81e00a7c
MC <Malckira@gmail.com>
2022-05-11 15:09:40 +0200

    fix

--- frontend/src/Components/Dropdown/Dropdown.jsx
+++ frontend/src/Components/Dropdown/Dropdown.jsx
@@ -1,30 +1,25 @@
-import "../Dropdown/dropdown.css"
-function Dropdown({ name, datas, onChange, children, placeholder}) {
-    return (
-      <div className="input dropdown">
-        <label className="label_dropdown" htmlFor={name}>
-          {name}
-        </label>
+import "../Dropdown/dropdown.css";
 
-        <select
-          className="input_dropdown"
-          name={name}
-          id={name}
-          onChange={onChange}
-          placeholder={placeholder}
-        >
-          {datas.map((data) => (
-            <option key={data.name} value={data.value}>
-              {data.name}
-            </option>
-          ))}
+function Dropdown({ name, datas, onChange }) {
+  return (
+    <div className="input dropdown">
 
-        </select>
+      <label className="label_dropdown" htmlFor={name}>
+        {name}
+      </label>
 
-        {children}
-        
-      </div>
-    );
-  }
-  
-  export default Dropdown;
+      <select className="input_dropdown" name={name} id={name} onChange={onChange}>
+
+        {datas.map((data) => (
+          <option key={data.name} value={data.value}>
+            {data.name}
+          </option>
+        ))}
+
+      </select>
+
+    </div>
+  );
+}
+
+export default Dropdown;
