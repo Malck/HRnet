38da773f4149672fbe33c81e82ec52705128af16
MC <Malckira@gmail.com>
2022-05-16 14:32:39 +0200

    mock

--- frontend/src/Store/Slice.jsx
+++ frontend/src/Store/Slice.jsx
@@ -1,31 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import dataMock from "./datamock.json"
 const isMock = true;
-const dataMock = [
-  {
-    firstName: "Joe",
-    lastName: "Loko",
-    birthDay: "1996-05-06",
-    startDate: "2004-12-25",
-    street: "mokez",
-    city: "fedfell",
-    zip: "23",
-    state: "FL",
-    department: "Legal",
-    id: "11154156",
-  },
-  {
-    firstName: "Adil",
-    lastName: "Bou",
-    birthDay: "1992-05-06",
-    startDate: "2004-08-10",
-    street: "zadar",
-    city: "oprie",
-    zip: "12",
-    state: "AS",
-    department: "Sales",
-    id: "11154256",
-  },
-];
 
 const userSlice = createSlice({
   name: "userslice",
